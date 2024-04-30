ec607793828d66310d4ebf0cce92255c3a3a27b7
nicolasmorvant <nicolas.morvant@tuta.io>
2024-04-30 12:18:58 +0200

    Modification des éléments importés dans root

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
+    <Main />
+    <Footer />
   </React.StrictMode>
 );
 
