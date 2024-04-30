9262f7d8feb604a291a249d665346958136c0094
nicolasmorvant <nicolas.morvant@tuta.io>
2024-04-30 18:35:06 +0200

    Importation de la bdd et passage en jsx

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
-    <Main />
+    <Main />    
     <Footer />
   </React.StrictMode>
 );
