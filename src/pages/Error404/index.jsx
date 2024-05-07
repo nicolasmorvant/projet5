9b018530b8dbe89504b85a5803524635ecabfe6f
nicolasmorvant <nicolas.morvant@tuta.io>
2024-05-07 16:50:27 +0200

    Ajout br

--- src/pages/Error404/index.jsx
+++ src/pages/Error404/index.jsx
@@ -5,10 +5,14 @@ import { Link } from 'react-router-dom'
 const Error404 = () => {
   return (
 
-    <main className='error'>
-      <h2 className='error__titre'>404</h2>
-      <p className='error__message'>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/home" className='error__link'>Retourner sur la page d'accueil</Link>
+    <main className="main">
+
+      <section className='error'>
+        <h2 className='error__titre'>404</h2>
+        <p className='error__message'>Oups! La page que <br />vous demandez n'existe pas.</p>
+        <Link to="/" className='error__link'>Retourner sur la page d'accueil</Link>
+      </section>
+
     </main>
 
   )
